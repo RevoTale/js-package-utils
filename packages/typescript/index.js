f3f76a92bf29aad6ceed38abae4f3246bc74b626
Liakhovskyi Vladyslav <32598874+rusted-love@users.noreply.github.com>
2023-09-09 13:45:34 +0300

    Add basic rules for React and typescript

---
+++ packages/typescript/index.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "sourceType": "module",
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+    "rules": {
+        "no-console": [
+            "error",
+            {
+                "allow": ["warn", "error"]
+            }
+        ],
+        "no-else-return": "error",
+        "no-empty-function": "error",
+        "no-lonely-if": "error",
+        "@typescript-eslint/no-empty-function": "error",
+        "max-lines": "error",
+        "prefer-arrow-callback": [
+            "error",
+            {
+                "allowNamedFunctions": true
+            }
+        ],
+        "func-style": [
+            "error",
+            "expression",
+            {
+                "allowArrowFunctions": true
+            }
+        ],
+        "es5/no-typeof-symbol": "error",
+        "no-unused-vars": [
+            "warn",
+            {
+                "argsIgnorePattern": "^_"
+            }
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                "argsIgnorePattern": "^_"
+            }
+        ]
+    },
+}
